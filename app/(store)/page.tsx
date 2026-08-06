c5f1b470b127abeeac94eeac040eb90641f7596c
Youssef-Sabri <youssefwael993@yahoo.com>
2026-08-07 00:33:18 +0300

    perf: set 1-hour Edge CDN caching for home page to reduce mobile TTFB to sub-50ms

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   },
 };
 
-export const revalidate = 300;
+export const revalidate = 3600;
 
 export default async function Page() {
   const [{ categories, products, hierarchy }, banners] = await Promise.all([
