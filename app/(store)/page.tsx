84ffa8d70e261c0589c1f5c87a042f9ef82ad0ff
Youssef-Sabri <youssefwael993@yahoo.com>
2026-08-07 00:21:04 +0300

    perf: optimize ISR caching on home page to reduce TTFB on Edge CDN

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   },
 };
 
-export const revalidate = 60;
+export const revalidate = 300;
 
 export default async function Page() {
   const [{ categories, products, hierarchy }, banners] = await Promise.all([
